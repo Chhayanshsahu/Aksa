a8ac6556c73f43bfa30fca385bad276f12c31b9c
Niraj Dhore <dhoreniraj83@gmail.com>
2025-02-25 09:37:18 +0530

    new update

--- Aksa/Assets/Niraj/mazerunner/crazy.cs
+++ Aksa/Assets/Niraj/mazerunner/crazy.cs
@@ -7,38 +7,31 @@ using UnityEngine.UI;
 public class crazy : MonoBehaviour
 {
     public int keys = 0;
-    public float speed = 5.0f;
+    public float speed = 3.0f;
     public Text keyAmount;
     public Text youwin; // Ensure this is assigned in the Inspector
     public GameObject gate;
     public Text displayText; // Reference to the Text object
     public float displayTime = 2f; // Time to display the text
 
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
         youwin.gameObject.SetActive(false); // Hide the "You Win!!!" text at the start
+        rb2d = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0, speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.linearVelocity = movement * speed;
+
         if (keys == 3)
         {
             Destroy(gate);
@@ -65,22 +58,7 @@ public class crazy : MonoBehaviour
         if (collision.gameObject.CompareTag("okk"))
         {
             // Reverse the movement direction when colliding with an object tagged "okk"
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(speed * Time.deltaTime, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(-speed * Time.deltaTime, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Translate(0, -speed * Time.deltaTime, 0);
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(0, speed * Time.deltaTime, 0);
-            }
+            rb2d.linearVelocity = -rb2d.linearVelocity;
         }
 
         // Handle collision with objects tagged "prince"
@@ -89,22 +67,22 @@ public class crazy : MonoBehaviour
             youwin.text = "You Win!!!"; // Set the text
             youwin.gameObject.SetActive(true); // Make the text visible
         }
-        {
-            // Check if the collided object has the tag "key"
-            if (collision.gameObject.CompareTag("keys"))
-            {
-                // Show the text
-                if (displayText != null)
-                {
-                    displayText.gameObject.SetActive(true);
-                    Invoke("HideText", displayTime); // Hide the text after a delay
-                }
 
-                // Optionally, destroy the key object
-                Destroy(collision.gameObject);
+        // Check if the collided object has the tag "key"
+        if (collision.gameObject.CompareTag("keys"))
+        {
+            // Show the text
+            if (displayText != null)
+            {
+                displayText.gameObject.SetActive(true);
+                Invoke("HideText", displayTime); // Hide the text after a delay
             }
+
+            // Optionally, destroy the key object
+            Destroy(collision.gameObject);
         }
-    } // End of OnCollisionEnter2D
+    }
+
     private void HideText()
     {
         if (displayText != null)
@@ -112,4 +90,4 @@ public class crazy : MonoBehaviour
             displayText.gameObject.SetActive(false);
         }
     }
-} // End of crazy class
+}
