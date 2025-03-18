f00dc5e884e41d4404dc66d022874d6589751925
Chhayanshsahu <chhayanshsahu16@gmail.com>
2025-03-18 16:13:22 +0530

    1

--- Aksa/Assets/Niraj/pick up light/lightscript.cs
+++ Aksa/Assets/Niraj/pick up light/lightscript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FlashLight : MonoBehaviour
 {
-    private Light light;
+    private new Light light;
 
     void Start()
     {
--- Aksa/Assets/Niraj/pick up light/pickupanddrop.cs
+++ Aksa/Assets/Niraj/pick up light/pickupanddrop.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class PickupAndDrop : MonoBehaviour
 {
-    public GameObject camera;
-    float maxPickupDistance = 5;
-    GameObject itemCurrentlyHolding;
-    bool isHolding = false;
+    public Camera playerCamera; // Reference to the player's camera
+    public float maxPickupDistance = 5f; // Maximum distance to pick up an item
+    public Vector3[] holdPositions; // Array of positions relative to the player to hold items
+    public KeyCode pickupKey = KeyCode.P; // Key to pick up an item
+    public KeyCode dropKey = KeyCode.O; // Key to drop an item
+
+    private GameObject itemCurrentlyHolding; // The item currently being held
+    private bool isHolding = false; // Whether the player is currently holding an item
+    private int currentHoldPositionIndex = 0; // Index of the current hold position
 
     void Update()
     {
-        if (Input.GetKeyDown("e")) Pickup();
-        if (Input.GetKeyDown("q")) Drop();
+        if (Input.GetKeyDown(pickupKey)) Pickup();
+        if (Input.GetKeyDown(dropKey)) Drop();
     }
 
     void Pickup()
     {
         RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, maxPickupDistance))
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, maxPickupDistance))
         {
-            if (hit.transform.tag == "Item")
+            if (hit.transform.CompareTag("Item"))
             {
-                if (isHolding) Drop();
+                if (isHolding) Drop(); // Drop the current item if holding one
+
                 itemCurrentlyHolding = hit.transform.gameObject;
-                foreach (var c in hit.transform.GetComponentsInChildren<Collider>()) if (c != null) c.enabled = false;
-                foreach (var r in hit.transform.GetComponentsInChildren<Rigidbody>()) if (r != null) r.isKinematic = true;
+                SetItemPhysics(false); // Disable physics on the item
+
+                // Set the item's parent to the player and adjust its position
                 itemCurrentlyHolding.transform.parent = transform;
-                itemCurrentlyHolding.transform.localPosition = Vector3.zero;
+                itemCurrentlyHolding.transform.localPosition = holdPositions[currentHoldPositionIndex] ;
                 itemCurrentlyHolding.transform.localEulerAngles = Vector3.zero;
+
                 isHolding = true;
+                currentHoldPositionIndex = (currentHoldPositionIndex + 1) % holdPositions.Length; // Cycle through hold positions
             }
         }
     }
@@ -39,15 +48,29 @@ public class PickupAndDrop : MonoBehaviour
         if (itemCurrentlyHolding != null)
         {
             itemCurrentlyHolding.transform.parent = null;
-            foreach (var c in itemCurrentlyHolding.GetComponentsInChildren<Collider>()) if (c != null) c.enabled = true;
-            foreach (var r in itemCurrentlyHolding.GetComponentsInChildren<Rigidbody>()) if (r != null) r.isKinematic = false;
-            isHolding = false;
+            SetItemPhysics(true); // Enable physics on the item
 
+            // Position the item on the ground in front of the player
             RaycastHit hitDown;
             if (Physics.Raycast(transform.position, -Vector3.up, out hitDown))
             {
                 itemCurrentlyHolding.transform.position = hitDown.point + new Vector3(transform.forward.x, 0, transform.forward.z);
             }
+
+            isHolding = false;
+        }
+    }
+
+    void SetItemPhysics(bool enablePhysics)
+    {
+        foreach (var collider in itemCurrentlyHolding.GetComponentsInChildren<Collider>())
+        {
+            if (collider != null) collider.enabled = enablePhysics;
+        }
+
+        foreach (var rigidbody in itemCurrentlyHolding.GetComponentsInChildren<Rigidbody>())
+        {
+            if (rigidbody != null) rigidbody.isKinematic = !enablePhysics;
         }
     }
 }
